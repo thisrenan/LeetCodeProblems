18242d436f06ce65b8a01b85e6aa4ce609deec75
Renan Augusto Romig <renanromig@hotmail.com>
2025-04-07 16:34:51 -0300

    add second problem AddTwoNumbers

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,108 @@
+﻿using System.Xml.Linq;
+
+namespace LeetCode
+{
+    public static class AddTwoNumbers
+    {
+        /*
+            You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+            You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ 
+            Example 1:
+            Input: l1 = [2,4,3], l2 = [5,6,4]
+            Output: [7,0,8]
+            Explanation: 342 + 465 = 807.
+
+            Example 2:
+            Input: l1 = [0], l2 = [0]
+            Output: [0]
+
+            Example 3:
+            Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+            Output: [8,9,9,9,0,0,0,1]
+        */
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static ListNode AddTwoNumbersSolve(ListNode l1, ListNode l2)
+        {
+            int[] l1Values = new int[ListNodeLenght(l1)];
+            int[] l2Values = new int[ListNodeLenght(l2)];
+
+            l1Values = ListNodeValues(l1);
+            l2Values = ListNodeValues(l2);
+
+            int l1Final = l1Values.Select((t, i) => t * Convert.ToInt32(Math.Pow(10, l1Values.Length - i - 1))).Sum();
+            int l2Final = l2Values.Select((t, i) => t * Convert.ToInt32(Math.Pow(10, l2Values.Length - i - 1))).Sum();
+
+            int l3 = l1Final + l2Final;
+
+            int[] l3Values = l3.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+
+            return LinkNodeValues(l3Values);
+        }
+
+        public static int ListNodeLenght(ListNode l1)
+        {
+            ListNode node = l1;
+            int i = 0;
+            while (node.next != null)
+            {
+                i++;
+                node = node.next;
+            }
+
+            return i+1;
+        }
+
+        public static int[] ListNodeValues(ListNode l1)
+        {
+            ListNode node = l1;
+            int i = ListNodeLenght(l1);
+            int[] l1Values = new int[i];
+
+            for (int j = 0; j < i; j++)
+            {
+                l1Values[j] = node.val;
+                node = node.next;
+            }
+
+            return l1Values;
+        }
+
+        public static ListNode LinkNodeValues(int[] values)
+        {
+            if (values.Length > 0)
+            {
+                if (values.Length == 1)
+                {
+                    return new ListNode(values[0], null);
+                }
+                else
+                {
+                    ListNode first = new ListNode(values[0], null);
+
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        ListNode next = new ListNode(values[i], first);
+                        first = next;
+                    }
+
+                    return first;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@
 using LeetCode;
 
 Console.WriteLine("Hello, World! This is a project to practice and solve LeetCodeProblems");
+Console.WriteLine("\nLets start with 1. Two Sum: ");
 
 int[] result = new int[2];
 result = TwoSum.TwoSumSolve([3, 2, 4], 6);
-if (result[0] == 1 & result[1] == 2) {
+if (result[0] == 1 & result[1] == 2)
+{
     Console.WriteLine("TwoSum validated [3, 2, 4], 6 == [1,2]");
 }
 else
@@ -21,4 +23,58 @@ if (result[0] == 0 & result[1] == 1)
 else
 {
     Console.WriteLine("TwoSum failed");
+}
+
+Console.WriteLine("\nLets go for 2. Add Two Numbers: ");
+
+int[] l1 = new int[] { 2, 4, 3 };
+int[] l2 = new int[] { 5, 6, 4 };
+
+AddTwoNumbers.ListNode listNode1 = AddTwoNumbers.LinkNodeValues(l1);
+AddTwoNumbers.ListNode listNode2 = AddTwoNumbers.LinkNodeValues(l2);
+AddTwoNumbers.ListNode listNode3 = AddTwoNumbers.AddTwoNumbersSolve(listNode1, listNode2);
+int[] lResult = AddTwoNumbers.ListNodeValues(listNode3);
+
+
+if (lResult[0] == 7 & lResult[1] == 0 & lResult[2] == 8)
+{
+    Console.WriteLine("AddTwoNumbers validated [2, 4, 3], [5,6,4] == [7, 0, 8]");
+}
+else
+{
+    Console.WriteLine("AddTwoNumbers failed");
+}
+
+int[] l3 = new int[] { 0 };
+int[] l4 = new int[] { 0 };
+
+AddTwoNumbers.ListNode listNode4 = AddTwoNumbers.LinkNodeValues(l3);
+AddTwoNumbers.ListNode listNode5 = AddTwoNumbers.LinkNodeValues(l4);
+AddTwoNumbers.ListNode listNode6 = AddTwoNumbers.AddTwoNumbersSolve(listNode4, listNode5);
+lResult = AddTwoNumbers.ListNodeValues(listNode6);
+
+if (lResult[0] == 0)
+{
+    Console.WriteLine("AddTwoNumbers validated [0], [0] == [0]");
+}
+else
+{
+    Console.WriteLine("AddTwoNumbers failed");
+}
+
+int[] l5 = new int[] { 9, 9, 9, 9, 9, 9, 9 };
+int[] l6 = new int[] { 9, 9, 9, 9 };
+
+AddTwoNumbers.ListNode listNode7 = AddTwoNumbers.LinkNodeValues(l5);
+AddTwoNumbers.ListNode listNode8 = AddTwoNumbers.LinkNodeValues(l6);
+AddTwoNumbers.ListNode listNode9 = AddTwoNumbers.AddTwoNumbersSolve(listNode7, listNode8);
+lResult = AddTwoNumbers.ListNodeValues(listNode9);
+
+if (lResult[0] == 8 & lResult[1] == 9 & lResult[2] == 9 & lResult[3] == 9 & lResult[4] == 0 & lResult[5] == 0 & lResult[6] == 0 & lResult[7] == 1)
+{
+    Console.WriteLine("AddTwoNumbers validated [9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9] == [8,9,9,9,0,0,0,1]");
+}
+else
+{
+    Console.WriteLine("AddTwoNumbers failed");
 }
